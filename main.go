125ca7da8b7d02d86b455134b44811ee6eaed0c6
adrian-borovnik <adrian.borovnik@gmail.com>
2024-08-03 20:46:04 +0200

    Add scraping for coat of arms

--- main.go
+++ main.go
@@ -16,11 +16,29 @@ func main() {
 		if !strings.Contains(e.Text, "Municipality") {
 			return
 		}
+
+		err := c.Visit(e.Request.AbsoluteURL(e.Attr("href")))
+		if err != nil {
+			fmt.Println(err)
+		}
+
 		municipality := strings.ReplaceAll(e.Text, "Municipality of ", "")
 		municipality = strings.ReplaceAll(municipality, "Urban ", "")
 		municipalities = append(municipalities, municipality)
 		fmt.Println(municipality)
 	})
+
+	c.OnHTML(".infobox img", func(e *colly.HTMLElement) {
+		if !strings.Contains(e.Attr("alt"), "Coat") {
+			return
+		}
+
+		fmt.Println(e.Attr("alt"))
+	})
+
+	//c.OnRequest(func(r *colly.Request) {
+	//	fmt.Println(r.URL)
+	//})
 
 	err := c.Visit("https://en.wikipedia.org/wiki/Municipalities_of_Slovenia")
 	if err != nil {
