929a065b8653315453380cbeed0c21ddda7c17ac
adrian-borovnik <adrian.borovnik@gmail.com>
2024-08-05 09:23:39 +0200

    Add image download

--- main.go
+++ main.go
@@ -3,8 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"io"
+	"net/http"
+	"os"
 	"strings"
 )
+
+func downloadImage(url string, outFolder string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf(res.Status)
+	}
+
+	splitUrl := strings.Split(url, "/")
+	fileName := splitUrl[len(splitUrl)-1]
+
+	file, err := os.Create(outFolder + "/" + fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, res.Body)
+	return err
+}
 
 func main() {
 
@@ -28,12 +55,30 @@ func main() {
 		fmt.Println(municipality)
 	})
 
-	c.OnHTML(".infobox img", func(e *colly.HTMLElement) {
-		if !strings.Contains(e.Attr("alt"), "Coat") {
+	c.OnHTML(".infobox a[title]", func(e *colly.HTMLElement) {
+		title := e.Attr("title")
+		if !strings.Contains(title, "Coat") {
 			return
 		}
 
-		fmt.Println(e.Attr("alt"))
+		url := e.Request.AbsoluteURL(e.Attr("href"))
+		err := c.Visit(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	c.OnHTML(".fullImageLink img[src]", func(e *colly.HTMLElement) {
+		alt := e.Attr("alt")
+		if !strings.Contains(alt, "File:") {
+			return
+		}
+
+		url := e.Attr("src")
+		err := downloadImage(e.Request.AbsoluteURL(url), "./out")
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 
 	//c.OnRequest(func(r *colly.Request) {
